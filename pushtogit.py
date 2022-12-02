a03b264dac5e7e87d28a0e93bc245d652b1f9043
lzhbhlrpython <Liubaolin20070125@outlook.com>
2022-12-02 14:11:56 +0800

    Update at 2022-12-02 14:11:56.

--- pushtogit.py
+++ pushtogit.py
@@ -5,7 +5,7 @@ import time
 def push_to_git():
     repo = git.Repo(os.getcwd())
     repo.git.add(update=True)
-    repo.index.commit('Update at ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())+'. By'+__file__)
+    repo.index.commit('Update at ' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())+'.')
     origin = repo.remote(name='origin')
     origin.push()
 
